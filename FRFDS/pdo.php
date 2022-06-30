6c1808790b59360d1d1264f2349a413555cf1041
wwweilin <102499389+wwweilin@users.noreply.github.com>
2022-06-30 09:16:58 +0800

    Delete FRFDS directory

--- FRFDS/pdo.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=FRFDS','woo', '1234');
-// See the "errors" folder for details...
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
